1bdcf6b4116a4dd187b09405b88c22658fcae9c3
Christopher Nellis <christopher.t.nellis@gmail.com>
2018-10-14 13:30:35 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# charlotte-jug-2018
+# Spring into TDD!
+
+This is the repository for the code created during the Charlotte Java Users Group Meetup on October 17th, 2018.
---
+++ src/main/java/io/nellis/videogameinventory/VideogameInventoryApiApplication.java
@@ -0,0 +1,12 @@
+package io.nellis.videogameinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideogameInventoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideogameInventoryApiApplication.class, args);
+    }
+}
