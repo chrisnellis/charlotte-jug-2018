ec265dd8ccd2b0d53bc28895c9c9da2e2e9fe68c
Christopher Nellis <christopher.t.nellis@gmail.com>
2018-10-15 11:42:09 -0400

    Small refactor

--- src/main/java/io/nellis/videogameinventory/repository/VideogameRepo.java
+++ src/main/java/io/nellis/videogameinventory/repository/VideogameRepo.java
@@ -1,6 +1,6 @@
 package io.nellis.videogameinventory.repository;
 
-import io.nellis.videogameinventory.model.VideogameEntity;
+import io.nellis.videogameinventory.model.entity.VideogameEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VideogameRepo extends CrudRepository<VideogameEntity, Integer> {
